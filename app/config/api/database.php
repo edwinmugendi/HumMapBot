fe5361ab75e85b1b8cf24e7abc19fcc0aa6ee557
Edwin Mugendi <edwinmugendi@gmail.com>
2014-05-12 08:42:53 +0300

    First Push

--- app/config/api/database.php
+++ app/config/api/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'lv',
+			'database'  => 'lava',
 			'username'  => 'root',
-			'password'  => 'lavasapama54321',
+			'password'  => 'lava123',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => 'sp_',
