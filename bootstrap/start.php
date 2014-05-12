2e72350bd5809d98df247165a938c9798147bf63
Edwin Mugendi <edwinmugendi@gmail.com>
2014-05-12 09:21:25 +0300

    First Push

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
     'local' => array('*sapama*'),
-    'api' => array('http://lavaapi.qwkpy*'),
+    'api' => array('*Lava*'),
         ));
 
 /*
